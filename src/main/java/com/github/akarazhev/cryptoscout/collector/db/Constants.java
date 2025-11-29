9b4e2bf1038375ddf6e9240788426242f5fe1c8e
Andrey Karazhev <karazhev@gmail.com>
2025-11-30 01:05:56 +0300

    Refactored constants

--- src/main/java/com/github/akarazhev/cryptoscout/collector/db/Constants.java
+++ src/main/java/com/github/akarazhev/cryptoscout/collector/db/Constants.java
@@ -29,8 +29,18 @@ public final class Constants {
         throw new UnsupportedOperationException();
     }
 
-    public final static class CMC {
-        private CMC() {
+    public final static class Range {
+        private Range() {
+            throw new UnsupportedOperationException();
+        }
+
+        // Time range
+        static final int FROM = 1;
+        static final int TO = 2;
+    }
+
+    public final static class Cmc {
+        private Cmc() {
             throw new UnsupportedOperationException();
         }
 
@@ -115,10 +125,6 @@ public final class Constants {
         public static final String TA_LINEAR_ORDER_BOOK_200_TABLE = "crypto_scout.bybit_ta_linear_order_book_200";
         public static final String TA_LINEAR_ORDER_BOOK_1000_TABLE = "crypto_scout.bybit_ta_linear_order_book_1000";
         public static final String TA_LINEAR_ALL_LIQUIDATION_TABLE = "crypto_scout.bybit_ta_linear_all_liqudation";
-
-        // Time range
-        static final int FROM = 1;
-        static final int TO = 2;
 
         // Bybit LPL
         static final String LPL_INSERT = "INSERT INTO " + BYBIT_LPL_TABLE +
