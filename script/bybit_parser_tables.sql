7410ba38792c9ed6b5068f2f07317134a4ec6526
Andrey Karazhev <karazhev@gmail.com>
2025-11-30 19:25:53 +0300

    Updated lpl schema

--- script/bybit_parser_tables.sql
+++ script/bybit_parser_tables.sql
@@ -15,18 +15,17 @@ create TABLE IF NOT EXISTS crypto_scout.bybit_lpl (
     stake_begin_time TIMESTAMP WITH TIME ZONE NOT NULL,
     stake_end_time TIMESTAMP WITH TIME ZONE NOT NULL,
     trade_begin_time TIMESTAMP WITH TIME ZONE,
-    CONSTRAINT bybit_lpl_pkey PRIMARY KEY (id, stake_begin_time)
+    CONSTRAINT bybit_lpl_pkey PRIMARY KEY (id)
 );
 
 alter table crypto_scout.bybit_lpl OWNER TO crypto_scout_db;
-create index IF NOT EXISTS idx_bybit_lpl_stake_begin_time ON crypto_scout.bybit_lpl(stake_begin_time DESC);
-select public.create_hypertable('crypto_scout.bybit_lpl', 'stake_begin_time', chunk_time_interval => INTERVAL '1 day', if_not_exists => TRUE);
+create index IF NOT EXISTS idx_bybit_lpl_stake_end_time ON crypto_scout.bybit_lpl(stake_end_time DESC);
+create index IF NOT EXISTS idx_bybit_lpl_trade_begin_time ON crypto_scout.bybit_lpl(trade_begin_time DESC);
+select public.create_hypertable('crypto_scout.bybit_lpl', 'stake_begin_time', chunk_time_interval => INTERVAL '1 year', if_not_exists => TRUE);
 
 alter table crypto_scout.bybit_lpl set (
     timescaledb.compress,
     timescaledb.compress_segmentby = 'return_coin',
-    timescaledb.compress_orderby = 'stake_begin_time DESC, id DESC'
+    timescaledb.compress_orderby = 'stake_begin_time DESC'
 );
-select add_compression_policy('crypto_scout.bybit_lpl', interval '7 days');
-select add_reorder_policy('crypto_scout.bybit_lpl', 'idx_bybit_lpl_stake_begin_time');
-select add_retention_policy('crypto_scout.bybit_lpl', interval '730 days');
+select add_compression_policy('crypto_scout.bybit_lpl', interval '35 days');
