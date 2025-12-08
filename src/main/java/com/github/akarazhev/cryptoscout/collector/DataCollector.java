19f9184333270c9fa2735e5047dfd6a5ec35b6d3
Andrey Karazhev <karazhev@gmail.com>
2025-12-08 14:08:24 +0300

    Implemented gets methods

--- src/main/java/com/github/akarazhev/cryptoscout/collector/DataCollector.java
+++ src/main/java/com/github/akarazhev/cryptoscout/collector/DataCollector.java
@@ -110,6 +110,132 @@ public final class DataCollector extends AbstractReactive {
                                 whenResult(fgis ->
                                         publishResponse(command.source(), Method.CMC_PARSER_GET_FGI, fgis));
                     }
+
+                    // BybitCryptoCollector methods
+                    case Method.BYBIT_GET_KLINE_1M -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getKline1m(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(klines ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_KLINE_1M, klines));
+                    }
+
+                    case Method.BYBIT_GET_KLINE_5M -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getKline5m(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(klines ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_KLINE_5M, klines));
+                    }
+
+                    case Method.BYBIT_GET_KLINE_15M -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getKline15m(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(klines ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_KLINE_15M, klines));
+                    }
+
+                    case Method.BYBIT_GET_KLINE_60M -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getKline60m(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(klines ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_KLINE_60M, klines));
+                    }
+
+                    case Method.BYBIT_GET_KLINE_240M -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getKline240m(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(klines ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_KLINE_240M, klines));
+                    }
+
+                    case Method.BYBIT_GET_KLINE_1D -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getKline1d(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(klines ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_KLINE_1D, klines));
+                    }
+
+                    case Method.BYBIT_GET_TICKER -> {
+                        final var args = message.value();
+                        bybitCryptoCollector.getTicker(
+                                BybitCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(tickers ->
+                                        publishResponse(command.source(), Method.BYBIT_GET_TICKER, tickers));
+                    }
+
+                    // BybitTaCryptoCollector methods
+                    case Method.BYBIT_TA_GET_ORDER_BOOK_1 -> {
+                        final var args = message.value();
+                        bybitTaCryptoCollector.getOrderBook1(
+                                BybitTaCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(orderBooks ->
+                                        publishResponse(command.source(), Method.BYBIT_TA_GET_ORDER_BOOK_1, orderBooks));
+                    }
+
+                    case Method.BYBIT_TA_GET_ORDER_BOOK_50 -> {
+                        final var args = message.value();
+                        bybitTaCryptoCollector.getOrderBook50(
+                                BybitTaCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(orderBooks ->
+                                        publishResponse(command.source(), Method.BYBIT_TA_GET_ORDER_BOOK_50, orderBooks));
+                    }
+
+                    case Method.BYBIT_TA_GET_ORDER_BOOK_200 -> {
+                        final var args = message.value();
+                        bybitTaCryptoCollector.getOrderBook200(
+                                BybitTaCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(orderBooks ->
+                                        publishResponse(command.source(), Method.BYBIT_TA_GET_ORDER_BOOK_200, orderBooks));
+                    }
+
+                    case Method.BYBIT_TA_GET_ORDER_BOOK_1000 -> {
+                        final var args = message.value();
+                        bybitTaCryptoCollector.getOrderBook1000(
+                                BybitTaCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(orderBooks ->
+                                        publishResponse(command.source(), Method.BYBIT_TA_GET_ORDER_BOOK_1000, orderBooks));
+                    }
+
+                    case Method.BYBIT_TA_GET_PUBLIC_TRADE -> {
+                        final var args = message.value();
+                        bybitTaCryptoCollector.getPublicTrade(
+                                BybitTaCryptoCollector.Type.valueOf((String) args.get(0)),
+                                (String) args.get(1), toOdt(args.get(2)), toOdt(args.get(3))).
+                                whenResult(trades ->
+                                        publishResponse(command.source(), Method.BYBIT_TA_GET_PUBLIC_TRADE, trades));
+                    }
+
+                    case Method.BYBIT_TA_GET_ALL_LIQUIDATION -> {
+                        final var args = message.value();
+                        bybitTaCryptoCollector.getAllLiquidation(
+                                (String) args.get(0), toOdt(args.get(1)), toOdt(args.get(2))).
+                                whenResult(liquidations ->
+                                        publishResponse(command.source(), Method.BYBIT_TA_GET_ALL_LIQUIDATION, liquidations));
+                    }
+
+                    // BybitParserCollector methods
+                    case Method.BYBIT_PARSER_GET_LPL -> {
+                        final var args = message.value();
+                        bybitParserCollector.getLpl(toOdt(args.getFirst())).
+                                whenResult(lpls ->
+                                        publishResponse(command.source(), Method.BYBIT_PARSER_GET_LPL, lpls));
+                    }
                 }
             }
         }
@@ -139,9 +265,30 @@ public final class DataCollector extends AbstractReactive {
             throw new UnsupportedOperationException();
         }
 
+        // CmcParserCollector methods
         public static final String CMC_PARSER_GET_KLINE_1D = "cmcParser.getKline1d";
         public static final String CMC_PARSER_GET_KLINE_1W = "cmcParser.getKline1w";
         public static final String CMC_PARSER_GET_FGI = "cmcParser.getFgi";
+
+        // BybitCryptoCollector methods
+        public static final String BYBIT_GET_KLINE_1M = "bybit.getKline1m";
+        public static final String BYBIT_GET_KLINE_5M = "bybit.getKline5m";
+        public static final String BYBIT_GET_KLINE_15M = "bybit.getKline15m";
+        public static final String BYBIT_GET_KLINE_60M = "bybit.getKline60m";
+        public static final String BYBIT_GET_KLINE_240M = "bybit.getKline240m";
+        public static final String BYBIT_GET_KLINE_1D = "bybit.getKline1d";
+        public static final String BYBIT_GET_TICKER = "bybit.getTicker";
+
+        // BybitTaCryptoCollector methods
+        public static final String BYBIT_TA_GET_ORDER_BOOK_1 = "bybitTa.getOrderBook1";
+        public static final String BYBIT_TA_GET_ORDER_BOOK_50 = "bybitTa.getOrderBook50";
+        public static final String BYBIT_TA_GET_ORDER_BOOK_200 = "bybitTa.getOrderBook200";
+        public static final String BYBIT_TA_GET_ORDER_BOOK_1000 = "bybitTa.getOrderBook1000";
+        public static final String BYBIT_TA_GET_PUBLIC_TRADE = "bybitTa.getPublicTrade";
+        public static final String BYBIT_TA_GET_ALL_LIQUIDATION = "bybitTa.getAllLiquidation";
+
+        // BybitParserCollector methods
+        public static final String BYBIT_PARSER_GET_LPL = "bybitParser.getLpl";
     }
 
     static class Source {
