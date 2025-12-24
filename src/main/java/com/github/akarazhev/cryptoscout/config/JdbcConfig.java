4b613309d27804d16a630acdb7e7709991a2aedd
Andrey Karazhev <karazhev@gmail.com>
2025-12-24 11:18:09 +0300

    Renamed variables.

--- src/main/java/com/github/akarazhev/cryptoscout/config/JdbcConfig.java
+++ src/main/java/com/github/akarazhev/cryptoscout/config/JdbcConfig.java
@@ -35,8 +35,8 @@ import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.HIKAR
 import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.HIKARI_REGISTER_MBEANS;
 import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_BYBIT_BATCH_SIZE;
 import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_BYBIT_FLUSH_INTERVAL_MS;
-import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_CMC_BATCH_SIZE;
-import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_CMC_FLUSH_INTERVAL_MS;
+import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_CRYPTO_SCOUT_BATCH_SIZE;
+import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_CRYPTO_SCOUT_FLUSH_INTERVAL_MS;
 import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_PASSWORD;
 import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_URL;
 import static com.github.akarazhev.cryptoscout.config.Constants.JdbcConfig.JDBC_USERNAME;
@@ -83,12 +83,12 @@ public final class JdbcConfig {
         return AppConfig.getAsBoolean(HIKARI_REGISTER_MBEANS);
     }
 
-    public static int getCmcBatchSize() {
-        return AppConfig.getAsInt(JDBC_CMC_BATCH_SIZE);
+    public static int getCryptoScoutBatchSize() {
+        return AppConfig.getAsInt(JDBC_CRYPTO_SCOUT_BATCH_SIZE);
     }
 
-    public static long getCmcFlushIntervalMs() {
-        return AppConfig.getAsInt(JDBC_CMC_FLUSH_INTERVAL_MS);
+    public static long getCryptoScoutFlushIntervalMs() {
+        return AppConfig.getAsInt(JDBC_CRYPTO_SCOUT_FLUSH_INTERVAL_MS);
     }
 
     public static int getBybitBatchSize() {
